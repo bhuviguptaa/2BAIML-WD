aee3e866567ebd21d51ed21e7a4cd86b34b170ed
Bhuvi Gupta <bhuviguptaa12@gmail.com>
2026-08-25 11:21:27 +0530

    functions

---
+++ B.js
@@ -0,0 +1,22 @@
+let text = "something";
+console.log(text);
+let a = 30;
+let b = 40;
+if(a>b){
+    colsole.log("a is greater than b");
+}
+else{
+    console.log("b is greater than a");
+}
+function greet(fname, lname){
+    console.log("hello",fname ,lname);
+}
+greet("Bhuvi", "Gupta")
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+add(10, 20);
+function fullname(fname="bhuvi", lname="gupta"){
+    console.log("hello",fname ,lname);
+}
+fullname();
