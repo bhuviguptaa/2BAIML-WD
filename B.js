91a9de51294345bfeb66e618b73f62dedd8247dd
Bhuvi Gupta <bhuviguptaa12@gmail.com>
2026-08-25 11:37:23 +0530

    functions

--- B.js
+++ B.js
@@ -20,3 +20,13 @@ function fullname(fname="bhuvi", lname="gupta"){
     console.log("hello",fname ,lname);
 }
 fullname();
+function total(s1,s2,s3){
+    return s1+s2+s3;
+}
+function add(marks){
+    return (marks/300)*100;
+}
+let totalMarks = total(80, 90, 85);
+console.log(totalMarks);
+let percentage = add(totalMarks);
+console.log(percentage);
