9af56914256c3bbf8ee7b0e19ea222ebbbc7d2f7
Bhuvi Gupta <bhuviguptaa12@gmail.com>
2026-09-03 10:58:59 +0530

    string template

---
+++ stringtemp.js
@@ -0,0 +1,6 @@
+let FirstName = "bhuvi";
+let LastName = "Gupta";
+console.log("my first name is : " + FirstName);
+console.log("my last name is : " + LastName);
+console.log(`my first name is ${FirstName}`);
+console.log(`my name is : ${FirstName} ${LastName}`);
