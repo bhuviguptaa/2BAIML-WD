565946268a52927306951f26b11e4f439005c5f4
Bhuvi Gupta <bhuviguptaa12@gmail.com>
2026-09-10 12:22:10 +0530

    oops in js

---
+++ oops.js
@@ -0,0 +1,8 @@
+class Student {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+};
+let s1 = new Student("bhuvi", 19);
+console.log(s1);
--- tempCodeRunnerFile.javascript
+++
@@ -1,7 +0,0 @@
-let F = "firstname"
-let L = "lastname"
-let data={
-    [F]: "bhuvi",
-    [L]: "gupta"
-}
-console.log(data.firstname, data.lastname)
